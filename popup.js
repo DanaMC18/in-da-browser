cd750a1ff039b2676c428e19bb78d8f56b13b1d8
Dana Czinsky <danaczinsky@Danas-Air.home>
2017-07-06 16:40:48 -0400

    create popup

---
+++ popup.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+  document.querySelector('#about').addEventListener('click', function(){
+    window.open("https://github.com/DanaMC18/in-da-browser/tree/master");
+  });
+
+  document.querySelector('#feeback').addEventListener('click', function(){
+    window.open("https://github.com/DanaMC18/in-da-browser/issues");
+  })
+
+})
