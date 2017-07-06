4dfa105e892e629bd780e4dceedc5b67a7e1966e
Dana Czinsky <danaczinsky@Danas-Air.home>
2017-07-06 19:00:24 -0400

    remove img tag; add styling

--- camdawgz.js
+++ camdawgz.js
@@ -1,13 +1,37 @@
 let images = [
-  "beach", "carmen-san-diego", "death-row-records", "disney", "eiffel-tower",
-  "fresh-prince", "greys-anatomy", "hasselhoff", "honey-i-shrunk-the-kids",
-  "jesus-christ-superstar", "lamb-chop", "lightning", "lily-pads",
-  "little-miss-sunshine", "mean-girls", "mt-rushmore", "sixteen-candles",
-  "snakes-on-a-plane", "sound-of-music", "strife", "superbowl"
+  {title: "beach", orientation: "landscape"},
+  {title: "carmen-san-diego", orientation: "portrait"},
+  {title: "death-row-records", orientation: "portrait"},
+  {title: "disney", orientation: "portrait"},
+  {title: "eiffel-tower", orientation: "portrait"},
+  {title: "fresh-prince", orientation: "landscape"},
+  {title: "greys-anatomy", orientation: "landscape"},
+  {title: "hasselhoff", orientation: "landscape"},
+  {title: "honey-i-shrunk-the-kids", orientation: "portrait"},
+  {title: "jesus-christ-superstar", orientation: "portrait"},
+  {title: "lamb-chop", orientation: "portrait"},
+  {title: "lightning", orientation: "landscape"},
+  {title: "lily-pads", orientation: "landscape"},
+  {title: "little-miss-sunshine", orientation: "landscape"},
+  {title: "mean-girls", orientation: "portrait"},
+  {title: "mt-rushmore", orientation: "landscape"},
+  {title: "sixteen-candles", orientation: "portait"},
+  {title: "snakes-on-a-plane", orientation: "portait"},
+  {title: "sound-of-music", orientation: "portait"},
+  {title: "strife", orientation: "landscape"},
+  {title: "superbowl", orientation: "portait"},
+  {title: "TMNT", orientation: "portait"},
+  {title: "waldo1", orientation: "portait"},
+  {title: "waldo2", orientation: "portait"},
+  {title: "wizard-of-oz", orientation: "landscape"}
 ]
 
 let displayImage = function() {
-  var image = images[Math.floor(Math.random() * images.length)];
+  let index = Math.floor(Math.random() * images.length);
+  let image = images[index];
+  let src = `images/camdawgz/${image.title}.jpg`;
   
-  document.querySelector('#main-image').src = `images/camdawgz/${image}.jpg`;
+  document.querySelector('body').background = src;
 }
+
+document.addEventListener('DOMContentLoaded', displayImage);
